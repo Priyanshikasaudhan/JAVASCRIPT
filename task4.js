d4841d0c8ce57992c1babc06f59b948b8947e43d
PRIYANSHI KASAUDHAN <147182966+Priyanshikasaudhan@users.noreply.github.com>
2024-05-23 21:35:14 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,36 @@
+//task1
+// let arr=[1,2,3,4,5,6,2,3,4];
+// let n = prompt("Enter any number: ");
+// console.log(arr)
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i] == n){
+//         arr.splice(i,1);
+//         console.log(arr)
+//     }
+// }
+//task2
+// let n = prompt("Enter the number: ");
+// let n=287152;
+// let count=0;
+// let copy = n;
+// while(copy>=0){
+//     count++;
+//     copy=Math.floor(copy/10);
+// }
+// console.log(count);
+//task3
+// let n=2865455;
+// let sum=0;
+// while(n>0){
+//     r = n%10;
+//     sum = Math.floor(sum+r);
+//     n=n/10;
+// }
+// console.log(sum);
+//task 4
+// let n = prompt("Enter the number:");
+// let fact =1;
+// for(let i=1;i<=n;i++){
+//     fact*= i;
+// }
+// console.log(`factorial of ${n} is ${fact}`);
