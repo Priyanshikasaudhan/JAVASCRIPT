9d2349fedca88d4df0cd23cc13c4c3e2d516cda5
PRIYANSHI KASAUDHAN <147182966+Priyanshikasaudhan@users.noreply.github.com>
2024-05-23 21:36:06 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,33 @@
+// let arr=[7,9,0,-1];
+// let n =prompt("Enter index: ");
+// let res = arr.slice(0,n);
+// console.log(res);
+// let last = arr.slice(n,arr.length);
+// console.log(last);
+//task3
+// let string = prompt("Enter a string: ")
+// if(string.length == 0){
+//     console.log("String is empty.");
+// }else{
+//     console.log("String is not empty.");
+// }
+//task4
+// let str = prompt("Enter a string: ");
+// let inx = prompt("Enter an index value: ")
+// if(str[inx] == str[inx].toLowerCase()){
+//     console.log("String in LowerCase.");
+// }else{
+//     console.log("String is not in LowerCase.")
+// }
+//task5
+// let s = prompt("Enter a string: ");
+// console.log(`Original String = ${s}`);
+// console.log(`After trim string = ${s.trim()}`);
+//task6
+let a=["prishu","Priyanshi",123,34,56];
+let value=prompt("Enter the value: ");
+if(a.indexOf(value) != -1){
+    console.log("Element exit in array.")
+}else{
+    console.log("Element is not exit in array.")
+}
