2bdb4f61ff5044d5740f63bfb730019a74e2c36e
PRIYANSHI KASAUDHAN <147182966+Priyanshikasaudhan@users.noreply.github.com>
2024-05-25 22:19:18 +0530

    Add files via upload

---
+++ day18.js
@@ -0,0 +1,40 @@
+//objects literals
+const student = {
+    name: "Priyanshi",
+    agr:24,
+    marks:97.4,
+    city:"Ayodhya"
+};
+console.log(student);
+//update
+student.city="Mumbai";
+console.log(student)
+//add
+student.gender = "Female";
+console.log(student);
+//change
+student.marks="A";
+console.log(student);
+//delete
+delete student.marks;
+console.log(student)
+// const item={
+//     price:1000.8,
+//     discount:50,
+//     colors : ["red","blue"]
+// };
+// console.log(item);
+// const post={
+//     username:"@Prishu",
+//     content:"#This is my first post",
+//     like:1650,
+//     reposts:5,
+//     tag:["@Prishu","@Princess" ,"@sriyas"]
+// };
+// const obj ={
+//     1:"a",
+//     2:"b",
+//     null:"c",
+//     undefined:"d",
+//     true:"e"
+// };console.log(obj); 
